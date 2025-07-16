6eaf05553e5a6917f8833bb6169df95b23c3c6c7
mrperfect-arxch <hackverse9@gmail.com>
2025-07-16 19:49:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ChatUI from './components/ChatUI';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4 bg-gray-900">
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 p-4">
       <ChatUI />
     </div>
   );
