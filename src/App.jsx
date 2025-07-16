e342dbf056e558e3342b4e022603e30158f67697
mrperfect-arxch <hackverse9@gmail.com>
2025-07-16 19:14:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatUI from './components/ChatUI';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 bg-gray-900">
+      <ChatUI />
+    </div>
+  );
+}
+
+export default App;
